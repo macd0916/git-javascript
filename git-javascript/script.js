e20c94019bed9cbb28df30c2cec3033316689277
Kaitrin MacDonald <kaitrinmacdonald@gmail.com>
2019-09-06 15:42:49 -0400

    adds javascript to the script.js file

--- git-javascript/script.js
+++ git-javascript/script.js
@@ -1,1 +1,4 @@
-name
+// Create the variable name and set its value to your name
+const yourName = 'Kaitrin'
+// Use console.log() and the variable name to log the following message to the console:
+console.log(`Hello, my name is ${yourName}.`)
