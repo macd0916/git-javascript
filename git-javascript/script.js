901e9877d69887cd20ac07cb37fd33cc90c14943
Kaitrin MacDonald <kaitrinmacdonald@gmail.com>
2019-09-06 15:55:29 -0400

    added many lines of code to script.js

--- git-javascript/script.js
+++ git-javascript/script.js
@@ -1,4 +1,22 @@
 // Create the variable name and set its value to your name
-const yourName = 'Kaitrin'
+const name = 'Kaitrin'
 // Use console.log() and the variable name to log the following message to the console:
-console.log(`Hello, my name is ${yourName}.`)
+console.log(`Hello, my name is ${name}.`)
+// Create the variable number and set its value to any positive integer
+const number = '3'
+//Multiply number by 2
+const result1 = function multiply (number, 2) {
+    return number * 2
+   }
+//Multiply the result by 5
+const result2 = function multiply (result1, 5) {
+     result1 * 5
+   }
+//Divide the result by number
+const result3 = function divide (result2, number) {
+    result2 / number
+  }
+//Subtract 7 from the result
+const result4 = function subtract (result2, 7) {
+    result3 - 7
+  }
