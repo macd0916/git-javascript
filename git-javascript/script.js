b3183cc75b8f0ec58f447dc91e78cf6588cc2500
macd0916 <43241197+macd0916@users.noreply.github.com>
2019-09-09 19:33:37 -0400

    fixed typos

--- git-javascript/script.js
+++ git-javascript/script.js
@@ -4,19 +4,5 @@ const name = 'Kaitrin'
 console.log(`Hello, my name is ${name}.`)
 // Create the variable number and set its value to any positive integer
 const number = '3'
-//Multiply number by 2
-const result1 = function multiply (number, 2) {
-    return number * 2
-   }
-//Multiply the result by 5
-const result2 = function multiply (result1, 5) {
-     result1 * 5
-   }
-//Divide the result by number
-const result3 = function divide (result2, number) {
-    result2 / number
-  }
-//Subtract 7 from the result
-const result4 = function subtract (result2, 7) {
-    result3 - 7
-  }
+//Multiply number by 2, multiply by 5, divide by number and subtract 7
+console.log(number * 2 * 5 / number - 7)
